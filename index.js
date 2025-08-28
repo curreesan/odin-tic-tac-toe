bc9eaea8ca6636e08a130c8d589a3a0e1c877bc6
curreesan <115443503+curreesan@users.noreply.github.com>
2025-08-28 11:31:44 +0530

    Add Gameboard module

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hi');
+const Gameboard = (function() {
+    let board = Array(9).fill('');
+
+    const getBoard = () => [...board];
+
+    const placeMark = (index, mark) => {
+        if ((index >= 0 && index < 9) && board[index] === '' && (mark === 'X' || mark === 'O')) {
+            board[index] = mark;
+            return true;
+        }
+        return false;
+    };
+
+    const isFull = () => board.every(cell => cell != '');
+
+    const reset = () => {
+        board = Array(9).fill('');
+    };
+
+    return {getBoard, placeMark, isFull, reset};
+})();
